711a3744f60285a4508bc3c5a5b8e1cb1ee4130a
javigarcias <javigarciaweb@gmail.com>
2021-10-12 03:21:11 +0200

    Implemento login estilo Antdesing

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,34 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-
+import { Form, Input, Button, Checkbox } from 'antd';
 
 import './Home.scss';
 
 const Home = () => {
 
     const history = useHistory();
+    const [login, setLogin] = useState({
+        email: "",
+        password: ""
+    });
 
     const handleSubmit = event => {
         event.preventDefault(); // para evitar refrescar la página
-        const user = {
-            email: event.target.email.value,
-            password: event.target.password.value
-        };
-        axios.post('http://localhost:3001/users/login',user)
-        .then(res=>{
-            localStorage.setItem("user", JSON.stringify(res.data));
-            history.push("/admin")
 
-        })
-        .catch(error=>console.log(error.response.data))
+        axios.post('http://localhost:3001/users/login', login)
+            .then(res => {
+                localStorage.setItem("user", JSON.stringify(res.data));
+                history.push("/admin")
+
+            })
+            .catch(error => console.log(error.response.data))
     }
+    const eventHandler = (ev) => {
+        setLogin({ ...login, [ev.target.name]: ev.target.value });
+    };
     return (
-        <form className="login-form" onSubmit={handleSubmit}>
-            <input type="email" name="email" required placeholder="Introduce email"/>
-            <input type="password" name="password" required placeholder="Introduce Password"/>
-            <button type="submit">Log in</button>
-        </form>
+        <div className="login">
+            <Form
+                name="login"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Email"
+                    name="email"
+
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
+                        },
+                    ]}
+                >
+                    <Input name="email" onChange={eventHandler} />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password name="password" onChange={eventHandler} />
+                </Form.Item>
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+
+        </div>
+
     )
 }
 
